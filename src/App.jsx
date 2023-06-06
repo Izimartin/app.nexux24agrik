9b40f09c67c2a9641e6b8510792e377e1f442e37
Izimartin <izimartinz@gmail.com>
2023-06-06 03:44:06 +0100

    Add App File

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import SignIn from "./components/auths/SignIn";
 
 function App() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return <SignIn />;
 }
 
 export default App;
